309405fd069f312bc599cefaedda0ca52cbd04c9
lyon <catgit.@catgit.com>
2021-08-13 18:17:51 +0800

    remove no use code

--- src/main.rs
+++ src/main.rs
@@ -7,23 +7,3 @@ fn main() {
     println!("{}", class_info_print);
 }
 
-pub fn add(a: i32, b: i32) -> i32 {
-    a + b
-}
-
-// 这个加法函数写得很差，本例中我们会使它失败。
-#[allow(dead_code)]
-fn bad_add(a: i32, b: i32) -> i32 {
-    a - b
-}
-
-#[cfg(test)]
-mod tests {
-    // 注意这个惯用法：在 tests 模块中，从外部作用域导入所有名字。
-    use super::*;
-
-    #[test]
-    fn test_add() {
-        assert_eq!(add(1, 2), 3);
-    }
-}
