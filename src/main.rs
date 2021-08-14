dbaa7601b4c178421eeae719d1591f83c3cdc6cb
lyon <catgit.@catgit.com>
2021-08-14 22:22:34 +0800

    change analizer to analyzer

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-mod analizer;
+mod analyzer;
 mod my_string;
 
 fn main() {
-    let class_info = analizer::ClassInfo::get_class_info_by_define(String::from("class Test(SuperTest):"));
+    let class_info = analyzer::ClassInfo::get_class_info_by_define(String::from("class Test(SuperTest):"));
     let class_info_print = class_info.unwrap().print_info();
     println!("{}", class_info_print);
 }
