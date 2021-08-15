06a44a3347e464a281bb8e268fc0f1578d032f3d
lyon <catgit.@catgit.com>
2021-08-15 15:22:48 +0800

    add analize_line, not tested

--- src/compiler.rs
+++ src/compiler.rs
@@ -1,35 +1,42 @@
 use crate::class_info::ClassInfo;
-pub struct Compiler {
-    pub class_now: &ClassInfo,
+pub struct Compiler<'a> {
     pub class_list: Vec<ClassInfo>,
+    pub class_now: &'a mut ClassInfo,
 }
 
-impl Compiler {
-    fn analize_line(&mut self, line: String) -> bool {
+impl<'a> Compiler<'a> {
+    fn analize_line(&'a mut self, line: String) -> bool {
         if line.starts_with("#") {
             return true;
         }
         if line.starts_with("class") {
-            self.class_list.push(match ClassInfo::new(&line) {
+            let mut class_now = match ClassInfo::new(&line) {
                 Some(s) => s,
                 None => return false,
-            })
+            };
+            self.class_list.push(class_now);
+            self.class_now = self.class_list.last_mut().unwrap();
+            return true;
         }
         if line.starts_with("    def ") {
             let line = line.strip_prefix("    ").unwrap().to_string();
-            self.class_list.push(match ClassInfo::new(&line) {
-                Some(s) => s,
-                None => return false,
-            })
+            self.class_now.push_method(line);
+            return true;
         }
-        if line.starts_with("    def ") {
+        if line.starts_with("    ")
+            && line.contains("(")
+            && line.contains(")")
+            && line.contains("=")
+        {
             let line = line.strip_prefix("    ").unwrap().to_string();
-            self.class_list.push(match ClassInfo::new(&line) {
-                Some(s) => s,
-                None => return false,
-            })
+            self.class_now.push_object(line);
+            return true;
         }
-
+        if line.starts_with("    ") && line.contains("(") && line.contains(")") {
+            let line = line.strip_prefix("    ").unwrap().to_string();
+            self.class_now.push_import(line);
+            return true;
+        }
         return false;
     }
 }
