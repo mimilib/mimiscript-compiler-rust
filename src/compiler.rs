95051ae15cf0308ff5b7e4992b559f663f332374
lyon <catgit.@catgit.com>
2021-08-15 15:43:22 +0800

    life time not good

--- src/compiler.rs
+++ src/compiler.rs
@@ -5,23 +5,23 @@ pub struct Compiler<'a> {
 }
 
 impl<'a> Compiler<'a> {
-    fn analize_line(&'a mut self, line: String) -> bool {
+    fn analize_line(&mut self, line: String) -> Option<Self>{
         if line.starts_with("#") {
-            return true;
+            return None;
         }
         if line.starts_with("class") {
             let mut class_now = match ClassInfo::new(&line) {
                 Some(s) => s,
-                None => return false,
+                None => return None,
             };
             self.class_list.push(class_now);
             self.class_now = self.class_list.last_mut().unwrap();
-            return true;
+            return Some(*self);
         }
         if line.starts_with("    def ") {
             let line = line.strip_prefix("    ").unwrap().to_string();
             self.class_now.push_method(line);
-            return true;
+            return Some(*self);
         }
         if line.starts_with("    ")
             && line.contains("(")
@@ -30,13 +30,28 @@ impl<'a> Compiler<'a> {
         {
             let line = line.strip_prefix("    ").unwrap().to_string();
             self.class_now.push_object(line);
-            return true;
+            return Some(*self);
         }
         if line.starts_with("    ") && line.contains("(") && line.contains(")") {
             let line = line.strip_prefix("    ").unwrap().to_string();
             self.class_now.push_import(line);
-            return true;
+            return Some(*self);
         }
-        return false;
+        return None;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_analyze() {
+        let mut compiler = Compiler{
+            class_now: &mut ClassInfo::new(&String::from("class Root(BaseObj):")).unwrap(),
+            class_list: vec![],
+        };
+        compiler.analize_line(String::from("class Test(SuperTest):"));
+        compiler.analize_line(String::from("    def test()"));
     }
 }
