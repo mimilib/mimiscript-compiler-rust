00eee0d7d52fad7035db97e430daa019e2a7c421
lyon <catgit.@catgit.com>
2021-08-15 12:25:47 +0800

    add compiler.rs

---
+++ src/compiler.rs
@@ -0,0 +1,4 @@
+use crate::class_info::ClassInfo;
+pub struct Compiler{
+    pub class_list: Vec<ClassInfo>,
+}
--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,12 @@ mod import_info;
 mod method_info;
 mod my_string;
 mod object_info;
+mod compiler;
 use class_info::*;
 use import_info::*;
 use method_info::*;
 use object_info::*;
+use compiler::*;
 
 fn main() {
     MethodInfo::new(
